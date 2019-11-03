d6ee493ad708c6cff9d7930b13c37251c1777359
Lucas Lee <lucasleeeeeeeee@gmail.com>
2019-11-03 15:21:16 +0800

    add object for post information
    for json serialization

---
+++ caroubot/PostInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caroubot
+{
+    class PostInfo
+    {
+        short NumberofPhotos { get; set; }
+        string Title { get; set; }
+        string Brand { get; set; }
+        //should be a link to a desciption file.
+        string Description { get; set; }
+        bool Used { get; set; }
+        int Price { get; set; }
+        bool MultipleUnits { get; set; }
+        bool MeetUp { get; set; }
+        bool Delivery { get; set; }
+        string Location { get; set; }
+
+    }
+}
